4f0a930b8b4e44a2df61cbe6ef11a69a7494b475
jakob <jakob@cf6e9589-b857-4ddd-b92b-997de31293b3>
2008-01-25 02:21:02 +0000

    - Added really basic text executable code - Added errorhandling macro
    git-svn-id: svn://localhost/lucie@2 cf6e9589-b857-4ddd-b92b-997de31293b3

---
+++ trunk/src/lucie.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <lua.h>
+#include <lauxlib.h>
+#include <lualib.h>
+
+#include "errorhandling.h"
+
+int main( int argc, char** argv )
+{
+    lua_State *L;
+
+    // Init the lua engine
+    L = luaL_newstate();
+    luaL_openlibs( L ); 
+
+    // Load the script for execution
+    LUACHECK( luaL_loadfile( L, argv[1] ) );
+
+    // Execute script
+    LUACHECK( lua_pcall( L, 0, LUA_MULTRET, 0 ) );
+
+    // Destroy the lua environment
+    lua_close( L );
+
+    return 0;
+}
