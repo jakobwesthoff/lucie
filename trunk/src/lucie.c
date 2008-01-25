bde2bef81c9009de6f17ea943d06dfa4a6c33554
jakob <jakob@cf6e9589-b857-4ddd-b92b-997de31293b3>
2008-01-25 02:36:14 +0000

    - Moved errorhandling macro to lucie.h - Added debuglog macro
    git-svn-id: svn://localhost/lucie@3 cf6e9589-b857-4ddd-b92b-997de31293b3

--- trunk/src/lucie.c
+++ trunk/src/lucie.c
@@ -4,7 +4,7 @@
 #include <lauxlib.h>
 #include <lualib.h>
 
-#include "errorhandling.h"
+#include "lucie.h"
 
 int main( int argc, char** argv )
 {
---
+++ trunk/src/lucie.h
@@ -0,0 +1,24 @@
+#ifndef LUCIE_H
+#define LUCIE_H
+
+#define LUACHECK(x) \
+    {                                                                                                     \
+        int error = x;                                                                                    \
+        if ( error )                                                                                      \
+        {                                                                                                 \
+             fprintf( stderr, "%s <%d>: %s\n\t%s\n", __FILE__, __LINE__, #x, lua_tostring( L, -1 ) );     \
+             lua_pop( L, 1 );                                                                             \
+             exit( EXIT_FAILURE );                                                                        \
+        }                                                                                                 \
+    }
+
+/* Debug functions are only declared if the DEBUG flag is set */
+#ifdef DEBUG
+    #define DEBUGLOG(fmt, ...) \
+        fprintf( stderr, fmt, __VA_ARGS__ )
+    
+#else
+    #define DEBUGLOG(...)
+#endif
+
+#endif
