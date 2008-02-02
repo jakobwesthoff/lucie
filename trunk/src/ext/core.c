6b3e2f5fccc8556371f7e4706217c17d80e5b2a1
jakob <jakob@cf6e9589-b857-4ddd-b92b-997de31293b3>
2008-02-02 19:15:55 +0000

    - Removed useless test functions from core extension
    git-svn-id: svn://localhost/lucie@25 cf6e9589-b857-4ddd-b92b-997de31293b3

--- trunk/src/ext/core.c
+++ trunk/src/ext/core.c
@@ -8,35 +8,6 @@
 
 #include "../lucie.h"
 
-
-int Lfoobar( lua_State *L ) 
-{
-    int bool;
-    const char *ch;
-    int len = 0;
-    double number;
-    PARAM_DOUBLE( number );
-    PARAM_BINARY_STRING( ch, len );
-    PARAM_BOOLEAN( bool );
-
-    printf( "core.foobar( %s, \"%s\" (%i), %f ) call\n", ( bool == 0 )?"false":"true", ch, len, number );
-    return 0;
-}
-
-int Lfoobar2( lua_State *L ) 
-{
-    const char* retval[] = { "foobar1", "foobar2", "foobar3" };
-    printf( "core.foobar2 call\n" );
-    RETURN_ARRAY( STRING, retval, 3 );
-    return 1;
-}
-
-int Lglobal_foobar( lua_State *L ) 
-{
-    printf( "global foobar call\n" );
-    return 0;
-}
-
 void print_padded_line( int padding, const char* fmt, ... ) 
 {
     const char* spacer = "  ";
@@ -133,10 +104,5 @@ int L_var_dump( lua_State* L )
 void register_extension( lua_State *L ) 
 {
     REGISTER_EXTENSION( "core", "Jakob Westhoff", "jakob@westhoffswelt.de" );
-    NAMESPACE_BEGIN( "core" );
-        REGISTER_NAMESPACE_FUNCTION( Lfoobar, foobar );
-        REGISTER_NAMESPACE_FUNCTION( Lfoobar2, foobar2 );
-    NAMESPACE_END();
-    REGISTER_GLOBAL_FUNCTION( Lglobal_foobar, foobar );
     REGISTER_GLOBAL_FUNCTION( L_var_dump, var_dump );
 }
